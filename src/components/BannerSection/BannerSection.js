69aed52ba88dc212f626633a758ec4b972ccc527
Akash A <akash@scube.co>
2024-04-06 13:22:54 +0530

    build issue fixed

--- src/components/BannerSection/BannerSection.js
+++ src/components/BannerSection/BannerSection.js
@@ -16,8 +16,8 @@ export default function BannerSection({ Class, data }) {
           <div className="banner-description">
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
+              industry. Lorem Ipsum has been the industry standard dummy text
+              ever since the 1500s when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
           </div>
